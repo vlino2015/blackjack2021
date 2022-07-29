12f203549a88b7c3a1087ddd2b53a24b31354bc5
Joy He <joyhe520@gmail.com>
2022-07-29 17:57:17 -0500

    deal button

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,447 @@
 
+let deckOfCards = [
+    {
+        'card':'2',
+        'value': 2,
+        'suit': 'club',
+        'image': 'images/2_of_clubs.png'
+     },
+
+     {
+        'card':'2',
+        'value': 2,
+        'suit': 'diamonds',
+        'image': 'images/2_of_diamonds.png'
+     },
+
+     {
+        'card':'2',
+        'value': 2,
+        'suit': 'hearts',
+        'image': 'images/2_of_hearts.png'
+     },
+
+     {
+        'card':'2',
+        'value': 2,
+        'suit': 'spades',
+        'image': 'images/2_of_spades.png'
+     },
+
+
+    {
+        'card':'3',
+        'value': 3,
+        'suit': 'club',
+        'image': 'images/3_of_clubs.png'
+     },
+
+     {
+        'card':'3',
+        'value': 3,
+        'suit': 'diamonds',
+        'image': 'images/3_of_diamonds.png'
+     },
+
+     {
+        'card':'3',
+        'value': 3,
+        'suit': 'hearts',
+        'image': 'images/3_of_hearts.png'
+     },
+
+     {
+        'card':'3',
+        'value': 3,
+        'suit': 'spades',
+        'image': 'images/3_of_spades.png'
+     },
+
+    {
+        'card':'4',
+        'value': 4,
+        'suit': 'club',
+        'image': 'images/4_of_clubs.png'
+     },
+
+     {
+        'card':'4',
+        'value': 4,
+        'suit': 'diamonds',
+        'image': 'images/4_of_diamonds.png'
+     },
+
+     {
+        'card':'4',
+        'value': 4,
+        'suit': 'hearts',
+        'image': 'images/4_of_hearts.png'
+     },
+
+     {
+        'card':'4',
+        'value': 4,
+        'suit': 'spades',
+        'image': 'images/4_of_spades.png'
+     },
+
+    {
+        'card':'5',
+        'value': 5,
+        'suit': 'club',
+        'image': 'images/5_of_clubs.png'
+     },
+
+     {
+        'card':'5',
+        'value': 5,
+        'suit': 'diamonds',
+        'image': 'images/5_of_diamonds.png'
+     },
+
+     {
+        'card':'5',
+        'value': 5,
+        'suit': 'hearts',
+        'image': 'images/5_of_hearts.png'
+     },
+
+     {
+        'card':'5',
+        'value': 5,
+        'suit': 'spades',
+        'image': 'images/5_of_spades.png'
+     },
+
+    {
+        'card':'6',
+        'value': 6,
+        'suit': 'club',
+        'image': 'images/6_of_clubs.png'
+     },
+
+     {
+        'card':'6',
+        'value': 6,
+        'suit': 'diamonds',
+        'image': 'images/6_of_diamonds.png'
+     },
+
+     {
+        'card':'6',
+        'value': 6,
+        'suit': 'hearts',
+        'image': 'images/6_of_hearts.png'
+     },
+
+     {
+        'card':'6',
+        'value': 6,
+        'suit': 'spades',
+        'image': 'images/6_of_spades.png'
+     },
+
+    {
+        'card':'7',
+        'value': 7,
+        'suit': 'club',
+        'image': 'images/7_of_clubs.png'
+     },
+
+     {
+        'card':'7',
+        'value': 7,
+        'suit': 'diamonds',
+        'image': 'images/7_of_diamonds.png'
+     },
+
+     {
+        'card':'7',
+        'value': 7,
+        'suit': 'hearts',
+        'image': 'images/7_of_hearts.png'
+     },
+
+     {
+        'card':'7',
+        'value': 7,
+        'suit': 'spades',
+        'image': 'images/7_of_spades.png'
+     },
+
+    {
+        'card':'8',
+        'value': 8,
+        'suit': 'club',
+        'image': 'images/8_of_clubs.png'
+     },
+
+     {
+        'card':'8',
+        'value': 8,
+        'suit': 'diamonds',
+        'image': 'images/8_of_diamonds.png'
+     },
+
+     {
+        'card':'8',
+        'value': 8,
+        'suit': 'hearts',
+        'image': 'images/8_of_hearts.png'
+     },
+
+     {
+        'card':'8',
+        'value': 8,
+        'suit': 'spades',
+        'image': 'images/8_of_spades.png'
+     },
+
+    {
+        'card':'9',
+        'value': 9,
+        'suit': 'club',
+        'image': 'images/9_of_clubs.png'
+     },
+
+     {
+        'card':'9',
+        'value': 9,
+        'suit': 'diamonds',
+        'image': 'images/9_of_diamonds.png'
+     },
+
+     {
+        'card':'9',
+        'value': 9,
+        'suit': 'hearts',
+        'image': 'images/9_of_hearts.png'
+     },
+
+     {
+        'card':'9',
+        'value': 9,
+        'suit': 'spades',
+        'image': 'images/9_of_spades.png'
+     },
+
+    {
+        'card':'10',
+        'value': 10,
+        'suit': 'club',
+        'image': 'images/10_of_clubs.png'
+     },
+
+     {
+        'card':'10',
+        'value': 10,
+        'suit': 'diamonds',
+        'image': 'images/10_of_diamonds.png'
+     },
+
+     {
+        'card':'10',
+        'value': 10,
+        'suit': 'hearts',
+        'image': 'images/10_of_hearts.png'
+     },
+
+     {
+        'card':'10',
+        'value': 10,
+        'suit': 'spades',
+        'image': 'images/10_of_spades.png'
+     },
+
+     {
+        'card':'ace',
+        'value1': 1,
+        'value2': 11,
+        'suit': 'clubs',
+        'image': 'images/ace_of_clubs.png'
+     },
+     {
+        'card':'ace',
+        'value1': 1,
+        'value2': 11,
+        'suit': 'diamonds',
+        'image': 'images/ace_of_diamonds.png'
+     },
+     {
+        'card':'ace',
+        'value1': 1,
+        'value2': 11,
+        'suit': 'hearts',
+        'image': 'images/ace_of_hearts.png'
+     },
+     {
+        'card':'ace',
+        'value1': 1,
+        'value2': 11,
+        'suit': 'spades',
+        'image': 'images/ace_of_spades.png'
+     },
+
+     {
+        'card':'jack',
+        'value': 10,
+        'suit': 'clubs',
+        'image': 'images/jack_of_clubs.png'
+     },
+     {
+        'card':'jack',
+        'value': 10,
+        'suit': 'diamonds',
+        'image': 'images/jack_of_diamonds.png'
+     },
+     {
+        'card':'jack',
+        'value': 10,
+        'suit': 'hearts',
+        'image': 'images/jack_of_hearts.png'
+     },
+     {
+        'card':'jack',
+        'value': 10,
+        'suit': 'spades',
+        'image': 'images/jack_of_spades.png'
+     },
+     {
+        'card':'king',
+        'value': 10,
+        'suit': 'clubs',
+        'image': 'images/king_of_clubs.png'
+     },
+     {
+        'card':'king',
+        'value': 10,
+        'suit': 'diamonds',
+        'image': 'images/king_of_diamonds.png'
+     },
+     {
+        'card':'king',
+        'value': 10,
+        'suit': 'hearts',
+        'image': 'images/king_of_hearts.png'
+     },
+     {
+        'card':'king',
+        'value': 10,
+        'suit': 'spades',
+        'image': 'images/king_of_spades.png'
+     },
+     {
+        'card':'queen',
+        'value': 10,
+        'suit': 'clubs',
+        'image': 'images/queen_of_clubs.png'
+     },
+     {
+        'card':'queen',
+        'value': 10,
+        'suit': 'diamonds',
+        'image': 'images/queen_of_diamonds.png'
+     },
+     {
+        'card':'queen',
+        'value': 10,
+        'suit': 'hearts',
+        'image': 'images/queen_of_hearts.png'
+     },
+     {
+        'card':'queen',
+        'value': 10,
+        'suit': 'spades',
+        'image': 'images/queen_of_spades.png'
+     },
+
+]
+
+let dealButton = document.querySelector('#deal-button');
+let imageDealer = document.querySelector('#dealer-hand');
+let imagePlayer = document.querySelector('#player-hand');
+let dealerPoint = document.querySelector('#dealer-points');
+let playerPoint = document.querySelector('#player-points');
+
+
+//add eventListener to deal button
+
+let dealerArr =[];
+let playerArr =[];
+
+
+dealButton.addEventListener('click', ()=>{
+    let deck = deckOfCards
+    
+    
+  
+    // console.log(deck);
+    for (let i = 0; i < 2; i++) {
+        let randomCard = deck[Math.floor(Math.random()*deck.length)];
+        let index = deck.findIndex(d => d===randomCard)
+        deck.splice(index,1);
+        playerArr.push(randomCard);
+
+        let randomCard2 = deck[Math.floor(Math.random()*deck.length)];
+        let index2 = deck.findIndex(d => d===randomCard)
+        deck.splice(index2,1);
+        dealerArr.push(randomCard2);
+
+      }
+      //adding points onto the screen
+      //dealer first
+
+      let dPoint= '';
+    //   console.log(dealerArr)
+    if(dealerArr.some(card => card.card === "ace")){
+        if(dealerArr[0].card === "ace" && dealerArr[1].card !== "ace"){
+            dPoint = dealerArr[0].value2 + dealerArr[1].value
+        }else if(dealerArr[0].card !== "ace" && dealerArr[1].card === "ace"){
+            dPoint = dealerArr[0].value + dealerArr[1].value2
+        }else{
+            dPoint = dealerArr[0].value1 + dealerArr[1].value2
+        }
+    }else{
+    
+      dPoint= dealerArr[0].value + dealerArr[1].value
+    //   console.log(dPoint)
+     
+    }
+
+    dealerPoint.innerHTML=dPoint
+  
+
+
+    //player next
+    let pPoint='';
+    if(playerArr.some(card => card.card === "ace")){
+        if(playerArr[0].card === "ace" && playerArr[1].card !== "ace"){
+            pPoint = playerArr[0].value2 + playerArr[1].value
+        }else if(playerArr[0].card !== "ace" && playerArr[1].card === "ace"){
+            pPoint = playerArr[0].value + playerArr[1].value2
+        }else{
+            pPoint = playerArr[0].value1 + playerArr[1].value2
+        }
+    }else{
+    
+      pPoint= playerArr[0].value + playerArr[1].value
+    //   console.log(dPoint)
+     
+    }
+
+    playerPoint.innerHTML=pPoint
+    
+    
+    
+    //add the card picture onto the screen
+
+        for(let i=0; i < dealerArr.length; i++){
+            imageDealer.innerHTML += `<img src="${dealerArr[i].image}" alt="">`
+        }
+        for(let i=0; i < playerArr.length; i++){
+            imagePlayer.innerHTML += `<img src="${playerArr[i].image}" alt="">`
+        }
+        
+    // run = false;
+
+})
